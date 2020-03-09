c8fdf70b8e6de9ccc50c3db4060aa7f7cdb0dae4
Fabricio Junio <j.fabricio6@gmail.com>
2020-03-09 00:09:00 -0300

    Swagger added

--- CitelProject.API/Controllers/CategoriesController.cs
+++ CitelProject.API/Controllers/CategoriesController.cs
@@ -20,30 +20,45 @@ namespace CitelProject.WebApi.Controllers
         }
 
 
+        /// <summary>
+        /// Retornar Lista de Categorias cadastradas
+        /// </summary>
         // GET: api/Categories
         public IEnumerable<CategoryViewModel> Get()
         {
             return _mapper.Map<IEnumerable<CategoryViewModel>>(_categoryApp.GetAll());
         }
 
+        /// <summary>
+        /// Retornar uma Categoria cadastrada
+        /// </summary>
         // GET: api/Categories/5
         public CategoryViewModel Get(int id)
         {
             return _mapper.Map<CategoryViewModel>(_categoryApp.GetById(id));
         }
 
+        /// <summary>
+        /// Cadastrar uma nova Categoria
+        /// </summary>
         // POST: api/Categories
         public void Post([FromBody]CategoryViewModel obj)
         {
             _categoryApp.Add(_mapper.Map<Category>(obj));
         }
 
+        /// <summary>
+        /// Editar uma Categoria cadastrada
+        /// </summary>
         // PUT: api/Categories/5
         public void Put(int id, [FromBody]CategoryViewModel obj)
         {
             _categoryApp.Update(_mapper.Map<Category>(obj));
         }
 
+        /// <summary>
+        /// Remover uma Categoria cadastrada
+        /// </summary>
         // DELETE: api/Categories/5
         public void Delete(int id)
         {
--- CitelProject.API/Controllers/ProductsController.cs
+++ CitelProject.API/Controllers/ProductsController.cs
@@ -19,30 +19,45 @@ namespace CitelProject.API.Controllers
             _mapper = AutoMapperConfig.Mapper;
         }
 
+        /// <summary>
+        /// Retornar Lista de Produtos cadastrados
+        /// </summary>
         // GET: api/Categories
         public IEnumerable<ProductViewModel> Get()
         {
             return _mapper.Map<IEnumerable<ProductViewModel>>(_productApp.GetAll());
         }
 
+        /// <summary>
+        /// Retornar um Produto cadastrado
+        /// </summary>
         // GET: api/Categories/5
         public ProductViewModel Get(int id)
         {
             return _mapper.Map<ProductViewModel>(_productApp.GetById(id));
         }
 
+        /// <summary>
+        /// Criar um novo Produto
+        /// </summary>
         // POST: api/Categories
         public void Post([FromBody]ProductViewModel obj)
         {
             _productApp.Add(_mapper.Map<Product>(obj));
         }
 
+        /// <summary>
+        /// Editar um novo Produto
+        /// </summary>
         // PUT: api/Categories/5
         public void Put(int id, [FromBody]ProductViewModel obj)
         {
             _productApp.Update(_mapper.Map<Product>(obj));
         }
 
+        /// <summary>
+        /// Remover um Produto cadastrado
+        /// </summary>
         // DELETE: api/Categories/5
         public void Delete(int id)
         {
